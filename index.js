6f2625c05fe08229e7f67d72b3f0d22ca8de1f93
Miguel Sanchez <miguelsanchez.inbox@gmail.com>
2023-03-08 15:43:13 -0500

    commit tree 9cf72ec278ec6d8fb47686e352ef799358abd3e9

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+//console.log(customerName); 
+
+const leastFavoriteCustomer = 'Rob Lopez';
+
+function upperCaseCustomerName(){
+
+    //console.log('the upper case customer name function has been called');
+
+     //customerName.toUpperCase(); 
+
+    customerName = customerName.toUpperCase(); 
+
+     //console.log('this is the customer name variable inside the uppercaseCustomerName function:', customerName.toUpperCase());
+}
+
+//upperCaseCustomerName(); 
+
+//console.log(customerName);
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Rachel Seno';
+}
